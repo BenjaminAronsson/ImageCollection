993835a49cffedf0ded9362a0cfb5029e95f54f7
Benjamin Aronsson <benjamin.aronsson@centiro.com>
2021-05-23 18:09:31 +0200

    added product  grid

---
+++ frontend/pages/products.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+  <product-grid :products="products"></product-grid>
+</template>
+
+<script>
+import Card from "~/components/Card";
+import ProductGrid from "../components/productGrid";
+
+export default {
+  name: "HomePage",
+  components: {
+    Card,
+    ProductGrid,
+  },
+  mounted() {
+    this.getProducts();
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    getProducts(context) {
+      fetch("http://localhost:5000/api/products/")
+        .then((response) => response.json())
+        .then((result) => {
+          this.products = result.products;
+        })
+        .catch((error) => {
+          console.warn(error.message);
+        });
+    },
+  },
+};
+</script>
